588e24341be22bbf2b0ec652fc02f615c2920678
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2026-01-20 14:28:34 +0100

    Schole

--- Problems_From_Books/PC_Assembly_Language/PCAL_1/PCAL_1_4_1_first.s
+++ Problems_From_Books/PC_Assembly_Language/PCAL_1/PCAL_1_4_1_first.s
@@ -1,11 +1,8 @@
-; Including some C I/O macros
-%include "asm_io.inc"
-
 ; An assembly file is separated into segments that have different purposes
 ; the .data segment is used to store data that we initialize
 segment .data
 
-outmsg db "Hello world!" 0 ; Creating 12 bytes containing characters, along with a 0 to cap it off (Like in C)
+outmsg db "Hello world!", 0 ; Creating 12 bytes containing characters, along with a 0 to cap it off (Like in C)
 
 ; The bss segment stores data we don't initialize
 segment .bss
@@ -14,6 +11,8 @@ segment .bss
 ; The .text segment is the "text" of the program, all of the actual code.
 segment .text
 
+    extern _printf    ; Getting printf from C
+
     global _asm_main ; Declaring the function we will write as global, so that the compiler can see it
 
 _asm_main:
@@ -21,8 +20,9 @@ _asm_main:
     pusha             ; Pushing the values of all general registers to the stack (preservation)
 
     ; Here comes the actual function haha (most of it is just preservation)
-    mov eax, outmsg   ; Moving the address of outmsg into eax. Note that like in C, outmsg is the adress of the first char in the string
-    call print_string ; Calling from C library. This prints from eax (i assume)
+    push outmsg  ; Pushing the message to the machine stack
+    call _printf ; Calling the function, which reads the message from the machine stack 
+    add esp, 8   ; Restoring the stack.
 
     popa              ; Popping all general registers back (preservation)
     mov eax, 0        ; C uses eax as the returning adress (C will read eax once this function returns)
--- Problems_From_Books/PC_Assembly_Language/PCAL_1/driver.c
+++ Problems_From_Books/PC_Assembly_Language/PCAL_1/driver.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 // Extremely simple C code that simply runs an assembly script
 int main()
 {
     int ret_status; 
     ret_status = asm_main(); // Running assembly function
+
+    printf("\nReturn status: %d\n",ret_status);
     return ret_status;
 }
